310c1f8669f5d409f5564986e9c7a42b7b262b5f
informaticscorporation <corporationinformatics@gmail.com>
2023-12-05 12:08:20 +0100

    completo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('butt').addEventListener('click', function() {
     console.log('ciao come stai?');
 });
+document.getElementById('rs').addEventListener('click', function() {
+    console.log('ciao sto benissimo');
+});
